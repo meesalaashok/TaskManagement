50329cd81cc9d1dc41d1b5112fb673450412d55f
BharathiParicharla <bharathiparicharla@gmail.com>
2024-02-18 15:46:41 +0530

    Committed By ashok kumar

---
+++ java_project/src/com/java_project/example/Task.java
@@ -0,0 +1,15 @@
+package com.java_project.example;
+
+public class Task {
+	String name;
+    String description;
+    String dueDate;
+	public Task(String name, String description, String dueDate) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.dueDate = dueDate;
+	}
+
+   
+}
